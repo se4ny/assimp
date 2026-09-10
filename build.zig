611b824cfa6e4aa22602b1604d7bd13a41c64b06
Sean Ryan <seantryan@protonmail.com>
2026-09-10 11:33:31 -0700

    added more build options for including shape formats

--- build.zig
+++ build.zig
@@ -7,8 +7,56 @@ pub fn build(b: *std.Build) void {
     const build_export = b.option(bool, "Build Export", "whether to build the export library") orelse false;
     const use_fbx = b.option(bool, "use_fbx", "whether to use the FBX importer") orelse false;
     const use_gltf = b.option(bool, "use_gltf", "whether to use the glTF importer") orelse false;
+    const use_3mf = b.option(bool, "use_3mf", "whether to use the 3MF importer") orelse false;
+    const use_ac = b.option(bool, "use_ac", "whether to use the AC importer") orelse false;
+    const use_amf = b.option(bool, "use_amf", "whether to use the AMF importer") orelse false;
+    const use_ase = b.option(bool, "use_amf", "whether to use the ASE importer") orelse false;
+    const use_assbin = b.option(bool, "use_assbin", "whether to use the Assbin importer") orelse false;
+    const use_assjson = b.option(bool, "use_assjson", "whether to use the Assjson importer") orelse false;
+    const use_assxml = b.option(bool, "use_assxml", "whether to use the Assxml importer") orelse false;
+    const use_b3d = b.option(bool, "use_b3d", "whether to use the B3D importer") orelse false;
+    const use_bvh = b.option(bool, "use_bvh", "whether to use the BVH importer") orelse false;
+    const use_blender = b.option(bool, "use_blender", "whether to use the Blender importer") orelse false;
+    const use_c4d = b.option(bool, "use_c4d", "whether to use the C4D importer") orelse false;
+    const use_cob = b.option(bool, "use_cob", "whether to use the COB importer") orelse false;
+    const use_csm = b.option(bool, "use_csm", "whether to use the CSM importer") orelse false;
+    const use_collada = b.option(bool, "use_collada", "whether to use the Collada importer") orelse false;
+    const use_dxf = b.option(bool, "use_dxf", "whether to use the DXF importer") orelse false;
+    const use_hmp = b.option(bool, "use_hmp", "whether to use the HMP importer") orelse false;
+    const use_ifc = b.option(bool, "use_ifc", "whether to use the IFC importer") orelse false;
+    const use_iqm = b.option(bool, "use_iqm", "whether to use the IQM importer") orelse false;
+    const use_irr = b.option(bool, "use_irr", "whether to use the IRR importer") orelse false;
+    const use_lwo = b.option(bool, "use_lwo", "whether to use the LWO importer") orelse false;
+    const use_lws = b.option(bool, "use_lws", "whether to use the LWS importer") orelse false;
+    const use_m3d = b.option(bool, "use_m3d", "whether to use the M3D importer") orelse false;
+    const use_md2 = b.option(bool, "use_md2", "whether to use the MD2 importer") orelse false;
+    const use_md3 = b.option(bool, "use_md3", "whether to use the MD3 importer") orelse false;
+    const use_md5 = b.option(bool, "use_md3", "whether to use the MD5 importer") orelse false;
+    const use_mdc = b.option(bool, "use_mdc", "whether to use the MDC importer") orelse false;
+    const use_mdl = b.option(bool, "use_mdl", "whether to use the MDL importer") orelse false;
+    const use_mmd = b.option(bool, "use_mmd", "whether to use the MMD importer") orelse false;
+    const use_ms3d = b.option(bool, "use_ms3d", "whether to use the MS3D importer") orelse false;
+    const use_ndo = b.option(bool, "use_ndo", "whether to use the NDO importer") orelse false;
+    const use_nff = b.option(bool, "use_nff", "whether to use the NFF importer") orelse false;
+    const use_off = b.option(bool, "use_off", "whether to use the OFF importer") orelse false;
+    const use_obj = b.option(bool, "use_obj", "whether to use the OBJ importer") orelse false;
+    const use_ogre = b.option(bool, "use_ogre", "whether to use the Ogre importer") orelse false;
+    const use_opengex = b.option(bool, "use_opengex", "whether to use the OpenGEX importer") orelse false;
+    const use_ply = b.option(bool, "use_ply", "whether to use the PLY importer") orelse false;
+    const use_q3bsp = b.option(bool, "use_q3bsp", "whether to use the Q3BSP importer") orelse false;
+    const use_q3d = b.option(bool, "use_q3d", "whether to use the Q3D importer") orelse false;
+    const use_raw = b.option(bool, "use_raw", "whether to use the RAW importer") orelse false;
+    const use_sib = b.option(bool, "use_sib", "whether to use the SIB importer") orelse false;
+    const use_smd = b.option(bool, "use_smd", "whether to use the SMD importer") orelse false;
+    const use_stl = b.option(bool, "use_stl", "whether to use the STL importer") orelse false;
+    const use_step = b.option(bool, "use_step", "whether to use the STEP importer") orelse false;
+    const use_terragen = b.option(bool, "use_terragen", "whether to use the Terragen importer") orelse false;
+    const use_vrml = b.option(bool, "use_vrml", "whether to use the VRML importer") orelse false;
+    const use_x = b.option(bool, "use_x", "whether to use the X importer") orelse false;
+    const use_x3d = b.option(bool, "use_x3d", "whether to use the X3D importer") orelse false;
+    const use_xgl = b.option(bool, "use_xgl", "whether to use the XGL importer") orelse false;
 
-    const upstream = b.dependency("assimp", .{});
+    const upstream = b.dependencby("assimp", .{});
 
     const mod = b.addModule("assimp", .{
         .target = target,
@@ -151,6 +199,162 @@ pub fn build(b: *std.Build) void {
     } else {
         mod.addCMacro("ASSIMP_BUILD_NO_GLTF_IMPORTER", "1");
         mod.addCMacro("ASSIMP_BUILD_NO_GLTF_EXPORTER", "1");
+    }
+
+    if (use_vrml) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_VRML_IMPORTER", "1");
+    }
+    if (use_terragen) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_TERRAGEN_IMPORTER", "1");
+    }
+    if (use_stl) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_STL_IMPORTER", "1");
+        mod.addCMacro("ASSIMP_BUILD_NO_STL_EXPORTER", "1");
+    }
+    if (use_step) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_STEP_IMPORTER", "1");
+    }
+    if (use_q3d) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_Q3D_IMPORTER", "1");
+    }
+    if (use_q3bsp) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_Q3BSP_IMPORTER", "1");
+    }
+    if (use_off) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_OFF_IMPORTER", "1");
+    }
+    if (use_nff) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_NFF_IMPORTER", "1");
+    }
+    if (use_ndo) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_NDO_IMPORTER", "1");
+    }
+    if (use_mmd) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_MMD_IMPORTER", "1");
+    }
+    if (use_md3) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_MD3_IMPORTER", "1");
+    }
+    if (use_lws) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_LWS_IMPORTER", "1");
+    }
+    if (use_dxf) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_DXF_IMPORTER", "1");
+    }
+    if (use_c4d) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_C4D_IMPORTER", "1");
+    }
+    if (use_assjson) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_ASSJSON_EXPORTER", "1");
+    }
+    if (use_csm) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_CSM_IMPORTER", "1");
+    }
+    if (use_smd) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_SMD_IMPORTER", "1");
+    }
+    if (use_assxml) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_ASSXML_IMPORTER", "1");
+        mod.addCMacro("ASSIMP_BUILD_NO_ASSXML_EXPORTER", "1");
+    }
+    if (use_opengex) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_OPENGEX_IMPORTER", "1");
+    }
+    if (use_collada) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_COLLADA_IMPORTER", "1");
+        mod.addCMacro("ASSIMP_BUILD_NO_COLLADA_EXPORTER", "1");
+    }
+    if (use_obj) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_OBJ_IMPORTER", "1");
+    }
+    if (use_mdl) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_MDL_IMPORTER", "1");
+    }
+    if (use_ms3d) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_MS3D_IMPORTER", "1");
+    }
+    if (use_sib) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_SIB_IMPORTER", "1");
+    }
+    if (use_cob) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_COB_IMPORTER", "1");
+    }
+    if (use_ase) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_ASE_IMPORTER", "1");
+    }
+    if (use_lwo) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_LWO_IMPORTER", "1");
+    }
+    if (use_md5) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_MD5_IMPORTER", "1");
+    }
+    if (use_b3d) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_B3D_IMPORTER", "1");
+    }
+    if (use_ac) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_AC_IMPORTER", "1");
+    }
+    if (use_mdc) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_MDC_IMPORTER", "1");
+    }
+    if (use_bvh) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_BVH_IMPORTER", "1");
+    }
+    if (use_hmp) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_HMP_IMPORTER", "1");
+    }
+    if (use_md2) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_MD2_IMPORTER", "1");
+    }
+    if (use_lwo) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_LWO_IMPORTER", "1");
+    }
+    if (use_xgl) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_XGL_IMPORTER", "1");
+    }
+    if (use_raw) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_RAW_IMPORTER", "1");
+    }
+    if (use_ply) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_PLY_IMPORTER", "1");
+        mod.addCMacro("ASSIMP_BUILD_NO_PLY_EXPORTER", "1");
+    }
+    if (use_irr) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_IRR_IMPORTER", "1");
+        mod.addCMacro("ASSIMP_BUILD_NO_IRRMESH_IMPORTER", "1");
+    }
+    if (use_blender) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_BLENDER_IMPORTER", "1");
+    }
+    if (use_x3d) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_X3D_IMPORTER", "1");
+    }
+    if (use_assbin) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_ASSBIN_IMPORTER", "1");
+        mod.addCMacro("ASSIMP_BUILD_NO_ASSBIN_EXPORTER", "1");
+    }
+    if (use_3mf) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_3MF_IMPORTER", "1");
+        mod.addCMacro("ASSIMP_BUILD_NO_3MF_EXPORTER", "1");
+    }
+    if (use_ogre) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_OGRE_IMPORTER", "1");
+    }
+    if (use_amf) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_AMF_IMPORTER", "1");
+    }
+    if (use_ifc) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_IFC_IMPORTER", "1");
+    }
+    if (use_iqm) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_IQM_IMPORTER", "1");
+    }
+    if (use_m3d) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_M3D_IMPORTER", "1");
+        mod.addCMacro("ASSIMP_BUILD_NO_M3D_EXPORTER", "1");
+    }
+    if (use_x) {} else {
+        mod.addCMacro("ASSIMP_BUILD_NO_X_IMPORTER", "1");
     }
 
     inline for (comptime std.meta.declarations(libraries)) |ext_lib| {
